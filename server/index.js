7782ea9ece5279028d426da1843e73e85caaf0ca
Shirsha Sengupta <shizzzzzzz7@gmail.com>
2023-06-09 15:30:33 +0530

    Adding Schema

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express= require("express");
 const mongoose = require("mongoose");
 const dotenv= require("dotenv");
 const {connection} = require("./db/connection");
+const Developer = require("./models/developerSchema");
 
 
 
