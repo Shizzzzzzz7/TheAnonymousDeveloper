ca8d2a35dbb563044ca2d06b89310f9801bcff62
Shirsha Sengupta <workofshirsha@gmail.com>
2023-06-14 17:29:36 +0530

    Adding 404 Page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import ContactUs from "./components/ContactUs";
+import NotFound from "./components/404Page";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route path="/contact" element={<ContactUs/>}/>
       <Route path="/signin" element={<SignIn/>}/>
       <Route path="/signup" element={<SignUp/>}/>
+      <Route path="*" element={<NotFound/>}/>
     </Routes>
     </>
   );
---
+++ client/src/components/404Page.js
@@ -0,0 +1,11 @@
+const NotFound= ()=>{
+
+    return(<>
+    <h1>404 Error!!!</h1>
+    <h3>Page Not Found</h3>
+
+    </>);
+}
+
+
+export default NotFound;
