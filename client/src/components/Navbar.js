93eb29ba4882ee1f5d88da0414dd2bcea679274f
Shirsha Sengupta <workofshirsha@gmail.com>
2023-06-14 10:18:22 +0530

    Modifying Client

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import {Link} from "react-router-dom";
+
+function Navbar(){
+
+    return(
+        <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/navbar">Navbar</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/about">About</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/signin">SignIn</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/signup">SignUp</Link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+        </>
+    );
+}
+
+export default Navbar;
