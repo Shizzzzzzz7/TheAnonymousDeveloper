9e174a0fc0967f38669a746e7b0006c4b4d04a56
Shirsha Sengupta <workofshirsha@gmail.com>
2023-06-15 18:16:08 +0530

    Adding Hooks

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,13 +1,32 @@
+import { useState } from "react";
+
 function SignUp(){
+
+    const [devDet, setDet]= useState({
+        name:"",
+        email:"",
+        phone:"",
+        work:"",
+        password:"",
+        cpassword:""
+    
+    });
+
+    function getFormData(e){
+        const {name,value}= e.target;
+        setDet((prevFormDet)=>({...prevFormDet,[name]:value}));
+        console.log(devDet);
+        e.preventDefault(); 
+    }
     return(
         <>
-            <form action="">
-                <input type="text" name="name" autoComplete="off" placeholder="Your Name"/><br />
-                <input type="text" name="email" autoComplete="off" placeholder="Your Email"/><br />
-                <input type="number" name="phone" autoComplete="off" placeholder="Your Number"/><br />
-                <input type="text" name="work" autoComplete="off" placeholder="Your Profession"/><br />
-                <input type="password" name="password" autoComplete="off" placeholder="Password"/><br />
-                <input type="password" name="cpassword" autoComplete="off" placeholder="Confirm Your Password"/><br />
+            <form >
+                <input type="text" name="name" autoComplete="off" value={devDet.name} placeholder="Your Name" onChange={getFormData}/><br />
+                <input type="text" name="email" autoComplete="off" value={devDet.email} placeholder="Your Email" onChange={getFormData}/><br />
+                <input type="number" name="phone" autoComplete="off" value={devDet.phone} placeholder="Your Number" onChange={getFormData}/><br />
+                <input type="text" name="work" autoComplete="off" value={devDet.work} placeholder="Your Profession" onChange={getFormData}/><br />
+                <input type="password" name="password" autoComplete="off" value={devDet.password} placeholder="Password" onChange={getFormData}/><br />
+                <input type="password" name="cpassword" autoComplete="off" value={devDet.cpassword} placeholder="Confirm Your Password" onChange={getFormData}/><br />
                 <input type="submit" name="submit" />
             </form>
         </>
